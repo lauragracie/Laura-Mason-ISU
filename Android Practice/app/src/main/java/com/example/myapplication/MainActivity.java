00bbfe7220e1a9dccdc2c1532d9a88c64c950851
SyrupSnowman <54853386+SyrupSnowman@users.noreply.github.com>
2019-11-11 09:59:22 -0500

    added south building images and buttons

--- Android Practice/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android Practice/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
     Button bfloor2north;
     Button bfloor3north;
     Button bfloor4north;
+    Button bfloor1south;
+    Button bfloor2south;
+    Button bfloor3south;
+    Button bclear;
     ImageView floor1north;
     ImageView floor2north;
     ImageView floor3north;
     ImageView floor4north;
+    ImageView floor1south;
+    ImageView floor2south;
+    ImageView floor3south;
 
 
     @Override
@@ -34,10 +41,18 @@ public class MainActivity extends AppCompatActivity {
         bfloor2north = findViewById(R.id.bfloor2north);
         bfloor3north = findViewById(R.id.bfloor3north);
         bfloor4north = findViewById(R.id.bfloor4north);
+        bfloor1south = findViewById(R.id.bfloor1south);
+        bfloor2south = findViewById(R.id.bfloor2south);
+        bfloor3south = findViewById(R.id.bfloor3south);
+        bclear = findViewById(R.id.bclear);
+
         floor1north = findViewById(R.id.floor1north);
         floor2north = findViewById(R.id.floor2north);
         floor3north = findViewById(R.id.floor3north);
         floor4north = findViewById(R.id.floor4north);
+        floor1south = findViewById(R.id.floor1south);
+        floor2south = findViewById(R.id.floor2south);
+        floor3south = findViewById(R.id.floor3south);
 
         bfloor1north.setOnClickListener(new View.OnClickListener() {
             public void onClick (View view){
@@ -45,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
                 floor2north.setAlpha(0f);
                 floor3north.setAlpha(0f);
                 floor4north.setAlpha(0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(0f);
             }
         });
 
@@ -54,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 floor2north.setAlpha(1.0f);
                 floor3north.setAlpha(0f);
                 floor4north.setAlpha(0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(0f);
             }
         });
 
@@ -63,6 +84,9 @@ public class MainActivity extends AppCompatActivity {
                 floor2north.setAlpha(0f);
                 floor3north.setAlpha(1.0f);
                 floor4north.setAlpha(0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(0f);
             }
         });
 
@@ -72,6 +96,57 @@ public class MainActivity extends AppCompatActivity {
                 floor2north.setAlpha(0f);
                 floor3north.setAlpha(0f);
                 floor4north.setAlpha(1.0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(0f);
+            }
+        });
+
+        bfloor1south.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view){
+                floor1north.setAlpha(0f);
+                floor2north.setAlpha(0f);
+                floor3north.setAlpha(0f);
+                floor4north.setAlpha(0f);
+                floor1south.setAlpha(1.0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(0f);
+            }
+        });
+
+        bfloor2south.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view){
+                floor1north.setAlpha(0f);
+                floor2north.setAlpha(0f);
+                floor3north.setAlpha(0f);
+                floor4north.setAlpha(0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(1.0f);
+                floor3south.setAlpha(0f);
+            }
+        });
+
+        bfloor3south.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view){
+                floor1north.setAlpha(0f);
+                floor2north.setAlpha(0f);
+                floor3north.setAlpha(0f);
+                floor4north.setAlpha(0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(1.0f);
+            }
+        });
+
+        bclear.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view){
+                floor1north.setAlpha(0f);
+                floor2north.setAlpha(0f);
+                floor3north.setAlpha(0f);
+                floor4north.setAlpha(0f);
+                floor1south.setAlpha(0f);
+                floor2south.setAlpha(0f);
+                floor3south.setAlpha(0f);
             }
         });
 
