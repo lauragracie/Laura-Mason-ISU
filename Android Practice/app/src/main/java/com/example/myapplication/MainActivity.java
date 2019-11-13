bd5eaba935f6e791cee71733c731119622a73be2
SyrupSnowman <54853386+SyrupSnowman@users.noreply.github.com>
2019-11-13 08:57:11 -0500

    Added a "touch to drag" function for the maps
    Currently bugged to only work with one map/inconsistent

--- Android Practice/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android Practice/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.myapplication;
 
+import android.os.Build;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
+import android.widget.RelativeLayout;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
     Button bfloor1north;
     Button bfloor2north;
     Button bfloor3north;
@@ -28,7 +33,12 @@ public class MainActivity extends AppCompatActivity {
     ImageView floor1south;
     ImageView floor2south;
     ImageView floor3south;
+    private ViewGroup rootlayout;
 
+    int windowwidth;
+    int windowheight;
+    private int _xDelta;
+    private int _yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +63,24 @@ public class MainActivity extends AppCompatActivity {
         floor1south = findViewById(R.id.floor1south);
         floor2south = findViewById(R.id.floor2south);
         floor3south = findViewById(R.id.floor3south);
+
+        floor1north.setOnTouchListener(this);
+        floor2north.setOnTouchListener(this);
+        floor3north.setOnTouchListener(this);
+        floor4north.setOnTouchListener(this);
+        floor1south.setOnTouchListener(this);
+        floor2south.setOnTouchListener(this);
+        floor3south.setOnTouchListener(this);
+
+        rootlayout = (ViewGroup) findViewById(R.id.root);
+
+        rootlayout.post(new Runnable() {
+            @Override
+            public void run() {
+                windowwidth = rootlayout.getWidth();
+                windowheight = rootlayout.getHeight();
+            }
+        });
 
         bfloor1north.setOnClickListener(new View.OnClickListener() {
             public void onClick (View view){
@@ -150,6 +178,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                _xDelta = X - view.getLeft();
+                _yDelta = Y - view.getTop();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_DOWN:
+            case MotionEvent.ACTION_POINTER_UP:
+                // Do nothing
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) view
+                        .getLayoutParams();
+                // Image is centered to start, but we need to unhitch it to move it around.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    lp.removeRule(RelativeLayout.CENTER_HORIZONTAL);
+                    lp.removeRule(RelativeLayout.CENTER_VERTICAL);
+                } else {
+                    lp.addRule(RelativeLayout.CENTER_HORIZONTAL, 0);
+                    lp.addRule(RelativeLayout.CENTER_VERTICAL, 0);
+                }
+                lp.leftMargin = X - _xDelta;
+                lp.topMargin = Y - _yDelta;
+                // Negative margins here ensure that we can move off the screen to the right
+                // and on the bottom. Comment these lines out and you will see that
+                // the image will be hemmed in on the right and bottom and will actually shrink.
+                lp.rightMargin = view.getWidth() - lp.leftMargin - windowwidth;
+                lp.bottomMargin = view.getHeight() - lp.topMargin - windowheight;
+                view.setLayoutParams(lp);
+                break;
+        }
+        // invalidate is redundant if layout params are set or not needed if they are not set.
+//        mRrootLayout.invalidate();
+        return true;
     }
 
     @Override
